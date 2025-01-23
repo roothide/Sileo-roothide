0ab7cf883697094af28b6e094abd6d76e753847a
roothider <158083651+roothider@users.noreply.github.com>
2025-01-23 20:12:35 +0800

    add palera1n(roothide) support

--- Sileo/Backend/Root Wrapper/Jailbreak.swift
+++ Sileo/Backend/Root Wrapper/Jailbreak.swift
@@ -48,8 +48,6 @@ enum Jailbreak: String, Codable {
     
     // Fugu15
     case fugu15 = "Fugu15 (iOS 15)"
-    case dopamine15 = "Dopamine-roothide (iOS 15)"
-    case dopamine16 = "Dopamine-roothide (iOS 16)"
     
     // Bakera1n
     case bakera1n_rootless15 = "bakera1n Rootless (iOS 15)"
@@ -62,10 +60,18 @@ enum Jailbreak: String, Codable {
     case other = "Other"
     case simulator = "Simulator"
     
+    case dopamine15 = "Dopamine-roothide (iOS 15)"
+    case dopamine16 = "Dopamine-roothide (iOS 16)"
+    
     case BOOTSTRAP14 = "Bootstrap (iOS 14)"
     case BOOTSTRAP15 = "Bootstrap (iOS 15)"
     case BOOTSTRAP16 = "Bootstrap (iOS 16)"
     case BOOTSTRAP17 = "Bootstrap (iOS 17)"
+    
+    case palehide15 = "Palera1n-roothide (iOS 15)"
+    case palehide16 = "Palera1n-roothide (iOS 16)"
+    case palehide17 = "Palera1n-roothide (iOS 17)"
+    case palehide18 = "Palera1n-roothide (iOS 18)"
     
     
     fileprivate static func arch() -> String {
@@ -91,30 +97,54 @@ enum Jailbreak: String, Codable {
         return
         #endif
         
+        //check palehide first
+        let palehide = URL(fileURLWithPath: jbroot("/.installed_palera1n"))
+        if palehide.exists {
+            if #available(iOS 19.0, *) {
+                self = .other
+            } else if #available(iOS 18.0, *) {
+                self = .palehide18
+            } else if #available(iOS 17.0, *) {
+                self = .palehide17
+            } else if #available(iOS 16.0, *) {
+                self = .palehide16
+            } else if #available(iOS 15.0, *) {
+                self = .palehide15
+            } else  {
+                self = .other
+            }
+            return
+        }
         
         let dopamine = URL(fileURLWithPath: jbroot("/.installed_dopamine"))
         if dopamine.exists {
-            if #available(iOS 16.0, *) {
+            if #available(iOS 17.0, *) {
+                self = .other
+            } else if #available(iOS 16.0, *) {
                self = .dopamine16
-               return
             } else if #available(iOS 15.0, *) {
                 self = .dopamine15
-                return
+            } else  {
+                self = .other
             }
-        } else if #available(iOS 17.0.1, *) {
-            self = .other
             return
-        } else if #available(iOS 17.0, *) {
-            self = .BOOTSTRAP17
-            return
-        } else if #available(iOS 16.0, *) {
-            self = .BOOTSTRAP16
-            return
-        } else if #available(iOS 15.0, *) {
-            self = .BOOTSTRAP15
-            return
-        } else if #available(iOS 14.0, *) {
-            self = .BOOTSTRAP14
+        }
+        
+        let bootstrap = URL(fileURLWithPath: jbroot("/.thebootstrapped"))
+        if bootstrap.exists {
+            if #available(iOS 17.0.1, *) {
+                self = .other
+            } else if #available(iOS 17.0, *) {
+                self = .BOOTSTRAP17
+            } else if #available(iOS 16.0, *) {
+                self = .BOOTSTRAP16
+            } else if #available(iOS 15.0, *) {
+                self = .BOOTSTRAP15
+            } else if #available(iOS 14.0, *) {
+                self = .BOOTSTRAP14
+            } else  {
+                self = .other
+            }
             return
         }
         
